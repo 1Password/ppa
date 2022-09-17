ddc7e835a39dea5481bbc328a93d87097b674ace
Jeffrey Goldberg <jeff@agilebits.com>
2022-09-17 12:26:31 -0500

    classier id anonymization

--- identifiers/keyed_hash.py
+++ identifiers/keyed_hash.py
@@ -1,6 +1,69 @@
 # Requires Python 3.7 or later.
 import hmac
+from hmac import HMAC
 import base64
+
+class Anonymizer:
+    def __init__(self, hash_key):
+        """Initialize a new Anonymizer with a key."""
+
+        # This is to compute keying and hasher creation just once
+        self.instance_hmac: HMAC = hmac.new(hash_key, digestmod='sha256')
+
+    def from_str(self, src: str) -> str:
+        """returns an anonymized form of the imput src."""
+
+        # We do NOT want to update the initialized hasher
+        # each time this is called, so we make copy.
+        try:
+             local_hmac: HMAC = self.instance_hmac.copy()
+        except AttributeError:
+            raise Exception("Anonymizer must be initialied before using")
+
+        local_hmac.update(src.encode('utf-8'))
+        anon_bytes: bytes = local_hmac.digest()
+        anon_truncated_bytes: bytes = truncate(anon_bytes)
+        anon_str: str = encode_to_string(anon_truncated_bytes)
+
+        return(anon_str)
+    
+
+# This demo deals a list of source data, and a list output.
+# In real usage Iterators may make more sense, but this demo.
+def anonymize_field(b5_field_name: str) -> list[str]:
+    """Returns a list of anonymized IDs for the B5 field."""
+
+    # get (or create) the secret hashing key
+    hash_key: bytes = get_hash_key_from_vault(field=b5_field_name)
+
+    # Create and initialize our keyed anonyimzer
+    anonymizer = Anonymizer(hash_key)
+
+    anonymized_ids: list[str] = []
+    for src in get_b5_field("email"):
+        anon_id = anonymizer.from_str(src)
+        anonymized_ids.append(anon_id)
+
+    return anonymized_ids
+
+def get_b5_field(field_name: str) -> list[str]:
+    """This would be able to read from the relevant B5 tables."""
+
+    # This sample code returns a list, but a more generic iterator may make
+    # sense when actually reading from a database.
+
+    # In this example code, the only field we know about is email
+    if field_name not in ["email"]:
+        # Given the potential for malicious input, we should not write out
+        # a potentially malicious field name.
+        raise ValueError("We can't fetch the field requested")
+
+    # our sample data is just a single short list
+    return [
+        'amelia@erhardt.fm',
+        'James.Hoffa@floor.lakemi.us',
+        'db_cooper@rocky_mtn.high.co.us',
+    ]
 
 # The hash_key is a high entropy secret that we will
 # pull from something like Secrets Automation.
@@ -8,8 +71,6 @@ import base64
 # but if we do use a password for this master secret,
 # it should be generated as a gibberish/random password of at
 # least 23 characters in length.
-
-
 def get_hash_key_from_vault(field: str) -> bytes:
     """Retrieves the secret hash key for field from a well protected place.
 
@@ -31,22 +92,15 @@ def get_hash_key_from_vault(field: str) -> bytes:
     # The secret needs to be better protected and managed.
     return b'XCGkaWfQQ9TQyfDLVKebYdH'
 
-
-# src is our field from b5 that we want to anonymize
-# We would be reading these from B5.
-field_from_b5: list[str] = [
-    'amelia@erhardt.fm',
-    'James.Hoffa@floor.lakemi.us',
-    'db_cooper@rocky_mtn.high.co.us',
-]
-
-
 def truncate(data: bytes, byte_length=15) -> bytes:
     """returns truncated data to min(byte_length, digest_size."""
 
     # 15 is a good default. It is collision safe for any plausible
     # number of things we are hashing, and it will produce cleaner results
     # with both base64 and base32 stringifications.
+    if byte_length < 12:
+        raise ValueError("byte_length must be at least 12")
+
     length = min(byte_length, len(data))
     return data[:length]
 
@@ -56,20 +110,9 @@ def encode_to_string(data: bytes) -> str:
     return base64.b64encode(data).decode(encoding="ascii")
 
 
-hash_key: bytes = get_hash_key_from_vault(field="email")
+def main():
+    for anon in anonymize_field("email"):
+        print(anon)
 
-# There is an optimization that can be done which
-# pre-computes and allocates some constant stuff outside of the loop.
-# If this loop needs optimizing, let me (Jeffrey Goldberg)
-# know.
-for src in field_from_b5:
-    hashed_src_bytes = hmac.digest(
-        key=hash_key,
-        msg=src.encode('utf-8'),
-        digest='sha256'
-    )
-
-    truncated_hash = truncate(hashed_src_bytes)
-    hashed_src = encode_to_string(truncated_hash)
-
-    print(hashed_src)
+if __name__ == "__main__":
+    main()
